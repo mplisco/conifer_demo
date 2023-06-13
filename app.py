e7a15cd421f0c1edbc6250d8e388d788d39064d2
Mike <mike@mymodus.co>
2023-06-13 14:45:20 -0500

    basic application setup

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask_smorest import Api
+from flask_migrate import Migrate
+
+from db import db
+
+def create_app(db_url=None):
+    app = Flask(__name__)
+    app.config["API_TITLE"] = "Flask Demo REST API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config[
+        "OPENAPI_SWAGGER_UI_URL"
+    ] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or "sqlite:///data.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    db.init_app(app)
+    migrate = Migrate(app, db)
+    api = Api(app)
+
+    @app.get("/")
+    def test_route():
+        return "Hello, World!"
+
+    with app.app_context():
+        db.create_all()
+
+    return app
